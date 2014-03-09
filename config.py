8b789c586276218627af6ae66a73023d9891bf68
WhiteWorld <ljq258@gmail.com>
2014-03-09 19:26:46 +0800

    add readme

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8 -*-
+POSTS_FILE_EXTENSION = '.md'
+PER_PAGE = 10
+
+FREEZER_DESTINATION = '../publish_blog'
+FREEZER_DESTINATION_IGNORE = ['.git*', "CNAME"]
+FREEZER_BASE_URL = 'http://blog.whiteworld.me/'
